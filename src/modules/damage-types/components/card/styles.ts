2e454fcb743322c398a7983bee1084ad2676d9dc
JoaoVitorPaludo <joaovpaludo@gmail.com>
2026-03-24 09:16:05 -0300

    style: Refatorando a estilização do card de tipos de dano e adicionando seção para descrição

--- src/modules/damage-types/components/card/styles.ts
+++ src/modules/damage-types/components/card/styles.ts
@@ -21,18 +21,9 @@ export const DamageTypesCardContent = styled(motion.div)`
   gap: 0.8rem;
   max-width: 60%;
   font-family: "Space Grotesk", sans-serif;
-  ul {
-    list-style: inside;
-    gap: 0.5rem;
-    display: flex;
-    flex-direction: column;
-  }
+
   h2 {
     font-size: 1.9rem;
-  }
-  span {
-    font-size: 1.2rem;
-    color: ${(props) => props.theme["green-500"]};
   }
 `;
 
@@ -42,4 +33,27 @@ export const DamageTypesCardNoDataContainer = styled.div`
   align-items: center;
   justify-content: center;
   font-size: 1.2rem;
+`;
+export const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+
+  h2 {
+    font-size: 1.4rem !important;
+    color: ${(props) => props.theme["green-500"]};
+    border-bottom: 1px solid ${(props) => props.theme.border};
+    padding-bottom: 0.3rem;
+  }
+
+  ul {
+    list-style: inside;
+    padding: 0;
+    margin: 0;
+
+    li {
+      margin-bottom: 0.8rem;
+      line-height: 1.5;
+    }
+  }
 `;
